01d79e9b600d4988d03059167aa06b8c8f9cf7d1
KristinaStepanova <kri18stina@gmail.com>
2019-06-24 01:02:18 +0300

    tabs

--- app/js/main.js
+++ app/js/main.js
@@ -54,5 +54,15 @@ $(function(){
         skin: "round"
     });
 
+    $('ul.tabs li').click(function(){
+      var tab_id = $(this).attr('data-tab');
+  
+      $('ul.tabs li').removeClass('current');
+      $('.tab-content').removeClass('current');
+  
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+
     var mixer = mixitup('.portfolio__content');
 });
